296d92981ddb8fa65b6e31b117b282a4e82a678e
Kingdon Barrett <kingdon.b@nerdland.info>
2018-01-21 23:35:20 +0000

    click through to the data

--- features/step_definitions/get_the_data_steps.rb
+++ features/step_definitions/get_the_data_steps.rb
@@ -10,7 +10,21 @@ Given("I see a list of links") do
 end
 
 When("I follow the first link") do
-  pending # Write code here that turns the phrase above into concrete actions
+  # Click the first link
+  link = first('li a')
+  link_text = link.text
+  link.click
+
+  # Follow the link to water-quality samples
+  find('a', text: 'Field/Lab water-quality samples', wait: 15).click
+
+  # Set options
+  find('select[name="rdb_inventory_output"]', wait: 15).
+    find('option[value=value]').select_option
+
+  # Submit
+  find('input#search_submit_button').click
+  save_page(link_text)
 end
 
 Then("I see a well-formed CSV document") do
