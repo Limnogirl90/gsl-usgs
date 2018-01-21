697d8261a5d6de1e212b2cb12484c75fa50844e9
Kingdon Barrett <kingdon.b@nerdland.info>
2018-01-21 22:34:09 +0000

    arrange for the links to be there with a fixture

--- features/support/env.rb
+++ features/support/env.rb
@@ -43,6 +43,20 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+Before do
+  fixtures_to_load =
+    %w[
+      datasets
+    ]
+
+  fixtures_file_spec = "{#{fixtures_to_load.join ','}}.yml"
+
+  ActiveRecord::FixtureSet.reset_cache
+  fixtures_folder = File.join(Rails.root, 'spec', 'fixtures')
+  fixtures = Dir[File.join(fixtures_folder, fixtures_file_spec)].map { |f| File.basename(f, '.yml') }
+  ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures)
+end
+
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
