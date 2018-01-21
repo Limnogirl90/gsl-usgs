2028aa43f31b22f48d1f04d7b2efeba86b2188b9
Kingdon Barrett <kingdon.b@nerdland.info>
2018-01-21 21:34:35 +0000

    test content

---
+++ features/step_definitions/get_the_data_steps.rb
@@ -0,0 +1,24 @@
+Given("I visit the app root") do
+  visit root_path
+end
+
+Given("I see a list of links") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I follow the first link") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I see a well-formed CSV document") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the CSV has a consistent number of columns throughout") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the number of columns is not zero") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
