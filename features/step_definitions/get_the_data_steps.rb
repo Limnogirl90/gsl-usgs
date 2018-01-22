704c35fa24eee6a650c5924c9b295357dd35d4b0
Kingdon Barrett <kingdon.b@nerdland.info>
2018-01-22 00:26:01 +0000

    refactor complex step for reuse

--- features/step_definitions/get_the_data_steps.rb
+++ features/step_definitions/get_the_data_steps.rb
@@ -1,5 +1,28 @@
 require "csv"
 
+def follow_link_please(nth)
+  links = all('li a')
+  link = links[nth]
+  link_text = link.text
+  @data_number = link_text
+  link.click
+
+  follow_link_for_csv_data(link_text)
+end
+
+def follow_link_for_csv_data(link_text)
+  # Follow the link to water-quality samples
+  find('a', text: 'Field/Lab water-quality samples', wait: 15).click
+
+  # Set options
+  find('select[name="rdb_inventory_output"]', wait: 15).
+    find('option[value=value]').select_option
+
+  # Submit
+  find('input#search_submit_button').click
+  save_page(link_text)
+end
+
 Given("I visit the app root") do
   visit root_path
 end
@@ -11,23 +34,9 @@ Given("I see a list of links") do
   end
 end
 
+
 When("I follow the first link") do
-  # Click the first link
-  link = first('li a')
-  link_text = link.text
-  @data_number = link_text
-  link.click
-
-  # Follow the link to water-quality samples
-  find('a', text: 'Field/Lab water-quality samples', wait: 15).click
-
-  # Set options
-  find('select[name="rdb_inventory_output"]', wait: 15).
-    find('option[value=value]').select_option
-
-  # Submit
-  find('input#search_submit_button').click
-  save_page(link_text)
+  follow_link_please(0)
 end
 
 Then("I see a well-formed CSV document") do
@@ -62,3 +71,31 @@ Then("the number of columns is not zero") do
   expect(@count_to_expect).not_to eq 0
 end
 
+When("I follow the rest of the links and collect their CSV") do
+  links = all('li a')
+
+  count = 0
+  links.each do |link|
+    link_text = link.text
+    @data_number = link_text
+
+
+    puts "(sub) I follow the #{count} link"
+    follow_link_please(count)
+
+    count += 1
+    visit root_path
+  end
+end
+
+Then("each of my CSVs are well-formed CSV document") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("each of my CSVs has a consistent number of columns throughout") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("each of my CSVs has a number of columns that is not zero") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
